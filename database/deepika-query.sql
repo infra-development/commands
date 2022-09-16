7baf93439f532c1a9ed91e901a29a61391afe641
Haresh Bhuriya <haresh.bhuriya@thalesgroup.com>
2022-09-16 22:43:32 +0530

    Sorting using awk

--- database/deepika-query.sql
+++ database/deepika-query.sql
@@ -46,3 +46,36 @@ from(SELECT table1.tac as tac,
           group by ctdatetime, device_tac, roamingflag, userid) as table1
      group by tac, ip_type, userid) as inner_table1
 group by tac, ip_type""").show
+
+
+select '3G' as rat,
+    dpiprotocol as data_service,
+    'ALL' as customer_group,
+    case roamingflag when 0 then 'Local' when 1 then 'Roaming' when 2 then 'Roaming' else 'Unknown' end as roamingflag,
+    count(distinct userid) as NoOfSubscribers,
+    (sum(downbytes * 8) / if(sum(duration) = 0, 1, sum(duration)) /1000) as down_binbandwidth,
+    (sum(upbytes * 8) / if(sum(duration) = 0, 1, sum(duration)) /1000) as up_binbandwidth ,
+    case when ctdatetime >= from_unixtime(unix_timestamp(to_timestamp('2022-08-03 00:00:00'))) and ctdatetime < from_unixtime(unix_timestamp(to_timestamp('2022-08-03 00:00:00')) + 3*3600) then 'bin1' end as bin
+from tb where ctdatetime >= '2022-08-03 00:00:00' and ctdatetime < '2022-08-04 00:00:00' and rat in (1000,1003) and lower(dpiprotocol) in ('facebook','flash','gmail','google') group by ctdatetime,dpiprotocol, roamingflag")
+
+
+
+select inner_table.time_period as Time_period,
+    inner_table.rat as Technology,
+    inner_table.data_service as Data_Service,
+    inner_table.customer_group as Customer_Group,
+    inner_table.ip_type as IP_Type,
+    max(case when inner_table.bin = 'bin1' then (inner_table.down_binbandwidth) / NoOfSubscribers else 0 end) as time_down_1
+from (select '2022-08-03 00:00:00' as time_period,
+        rat as rat,
+        dpiprotocol as data_service,
+        'ALL' as customer_group,
+        case roamingflag when 0 then 'Local' when 1 then 'Roaming' when 2 then 'Roaming' else 'Unknown' end as ip_type,
+        count(distinct userid) as NoOfSubscribers,
+        (sum(downbytes * 8) / if(sum(duration) = 0, 1, sum(duration)) /1000) as down_binbandwidth,
+        (sum(upbytes * 8) / if(sum(duration) = 0, 1, sum(duration)) /1000) as up_binbandwidth,
+        case when ctdatetime >= from_unixtime(unix_timestamp(to_timestamp('2022-08-03 00:00:00'))) and ctdatetime < from_unixtime(unix_timestamp(to_timestamp('2022-08-03 00:00:00')) + 3*3600) then 'bin1' else
+    from output_7s
+    where ctdatetime >= '2022-08-03 00:00:00' and ctdatetime < '2022-08-04 00:00:00' and lower(dpiprotocol) in ('facebook','flash','gmail','google')
+    group by rat, dpiprotocol, roamingflag, ctdatetime)
+    group by time_period, rat, Data_Service, Customer_Group, IP_Type
--- script/sort-awk.bash
+++ script/sort-awk.bash
@@ -1,11 +1,8 @@
 # -F is for field separator
-# Printing second column
+# Print multiple columns. Column number 2, 3, 4
 # unique values
 # default sorting
-cat bdr_data.csv | awk -F, '{print $2}' | sort | uniq
-
-# Print multiple columns. Column number 2, 3, 4
-cat bdr_data.csv | awk -F, '{print $2, $3, $4}'
+cat bdr_data.csv | awk -F, '{print $2, $3, $4}' | sort | uniq
 
 # Print number of fields in each line
 cat bdr_data.csv | awk -F, '{print NF}'
